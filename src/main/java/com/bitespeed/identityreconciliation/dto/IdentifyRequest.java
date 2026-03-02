5d8e6b20060a92a57cafd4a5f0dce54b759d3181
Gaurav Naike <gofo1622@gmail.com>
2026-03-02 16:25:27 +0530

    Add request and response DTOs

---
+++ src/main/java/com/bitespeed/identityreconciliation/dto/IdentifyRequest.java
@@ -0,0 +1,12 @@
+package com.bitespeed.identityreconciliation.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class IdentifyRequest {
+
+    private String email;
+    private String phoneNumber;
+}
---
+++ src/main/java/com/bitespeed/identityreconciliation/dto/IdentifyResponse.java
@@ -0,0 +1,22 @@
+package com.bitespeed.identityreconciliation.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class IdentifyResponse {
+
+    private ContactResponse contact;
+
+    @Data
+    @Builder
+    public static class ContactResponse {
+        private Long primaryContactId;
+        private List<String> emails;
+        private List<String> phoneNumbers;
+        private List<Long> secondaryContactIds;
+    }
+}
